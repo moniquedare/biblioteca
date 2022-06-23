d4d11db31f311d89459247b2b3f6d6f5741e2cd5
moniquedare <102301343+moniquedare@users.noreply.github.com>
2022-06-23 11:11:51 -0300

    Update users.js

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Estamos na rota users');
 });
 
+
+router.get('/sol', function(req, res, next) {
+  res.send('Estamos no verão!');
+});
+
+
+router.get('/frio', function(req, res, next) {
+  res.send('Estamos no inverno!');
+});
+
+router.get('/cool', function(req, res, next) {
+  res.send('você é tão legal')
+});
 module.exports = router;
