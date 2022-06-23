84bb537d142d75fda6f86ca9838f806cbb2f3afe
moniquedare <102301343+moniquedare@users.noreply.github.com>
2022-06-23 11:11:28 -0300

    Update index.js

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Estamos na rota users!');
 });
+
+
+router.get('/sol', function(req, res, next) {
+  res.send('Estamos no verão!');
+});
+
 
 module.exports = router;
